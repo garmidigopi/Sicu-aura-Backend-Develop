44d3b39fb30b8c488325a65e90fedc7f8ffc89d7
garmidi gopikrishna <105537998+garmidigopi@users.noreply.github.com>
2023-12-24 11:58:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const sign = document.getElementById('sign');
+const websiteURL = "reg.html";
+sign.addEventListener('click', () => {                            
+window.open(websiteURL, '_blank');
+});
---
+++ login.js
@@ -0,0 +1,5 @@
+const login = document.getElementById('login');
+const websiteURL = "chat.html";
+login.addEventListener('click', () => {                            
+window.open(websiteURL, '_blank');
+});
